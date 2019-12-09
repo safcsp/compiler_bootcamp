d26393c94d2d658cc7e3e0f501acaa6d2550399a
Ayman <ayman.alshanqiti@gmail.com>
2019-12-09 20:56:51 +0300

    organize our code into folders/files and make a new heighlighter

--- main.py
+++ main.py
@@ -1,167 +1,34 @@
 
-class Token:
-  def __init__(self,tid, value, category, position, line_number):
-    self.tid = tid
-    self.value = value
-    self.category = category
-    self.position = position
-    self.line_number = line_number
+from tokenizer import Tokenizer
 
-class Tokenizer:
-  def __init__(self, source_code):
-    self.position = -1
-    self.line_number = 1
-    self.step_keywords = ['let', 'var', 'int', 'float', 'string', 'boolean', 'if', 'else', 'for', 'while', 'end']
-    self.punctuations = {
-      '(' : 'left_paren',
-      ')' : 'right_paren',
-      '[' : 'left_square',
-      ']' : 'right_square',
-      ';' : 'semicolon',
-      ',' : 'colon',
-      '{' : 'left_curly',
-      '}' : 'right_curly',
-    }
-    self.source_code = source_code
-    self.length = len(self.source_code)
+source_code = ''
 
+with open('examples/main.stp') as stp:
+  source_code = stp.read(1024)
 
-  def is_eof(self):
-    return not self.position < self.length
-  
-  def is_peekable(self):
-    return (self.position + 1) < self.length
-  
-  def peek(self):
-    if self.is_peekable():
-      return self.source_code[self.position + 1]
-    return '\0'
-
-
-  def number_tokenizer(self):
-    character = self.source_code[self.position]
-    token = Token('integer_literal', character, 'literal', self.position, self.line_number)
-    self.position += 1
-    while not self.is_eof():
-      character = self.source_code[self.position]
-      if not character.isdigit():
-        self.position -= 1
-        break
-      else: #isdigit
-        token.value += character
-        self.position += 1
-
-    return token
-  
-  def identifier_tokenizer(self):
-    character = self.source_code[self.position]
-    token = Token('id', character, 'identifier', self.position, self.line_number)
-    self.position += 1
-    while not self.is_eof():
-      character = self.source_code[self.position]
-      if not (character.isalnum() or character == '_'):
-        self.position -= 1
-        break
-      else:
-        token.value += character
-        self.position += 1
-      
-    if token.value in self.step_keywords:
-      token.category = 'keyword'
-      token.tid = token.value + '_keyword'
-      return token
-
-    return token
-  
-  def comment_tokenizer(self):
-    character = self.source_code[self.position]
-    token = Token('comment', character, 'comment', self.position, self.line_number)
-    self.position += 1
-    while not self.is_eof():
-      character = self.source_code[self.position]
-      if character == '\n':
-        self.position -= 1
-        break
-      else: 
-        token.value += character
-        self.position += 1
-
-    return token
-  
-  def whitespace_tokenizer(self):
-    character = self.source_code[self.position]
-    token = Token('whitespace', character, 'whitespace', self.position, self.line_number)
-        
-    if character == '\n':
-      self.line_number += 1
-  
-    self.position += 1
-    while not self.is_eof():
-      character = self.source_code[self.position]
-      if not character.isspace():
-        self.position -= 1
-        break
-      else: #isspace
-        if character == '\n':
-          self.line_number += 1
-        token.value += character
-        self.position += 1
-
-    return token
-
-  def plus_tokenizer(self):
-    character = self.source_code[self.position]
-    token = Token('plus', character, 'operator', self.position, self.line_number)
-    peek_value = self.peek()
-    if peek_value == '+':
-      self.position += 1
-      token.value += self.source_code[self.position]
-      token.tid = 'plusplus'
-
-    return token
-  
-  def equal_tokenizer(self): 
-    character = self.source_code[self.position]
-    token = Token('assignment', character, 'operator', self.position, self.line_number)
-    if self.peek() == '=':
-      self.position += 1
-      token.value += self.source_code[self.position]
-      token.tid = 'equalequal'
-    return token
-
-  def punctuation_tokenizer(self): 
-    character = self.source_code[self.position]
-    tid = self.punctuations[character]
-    return Token(tid, character, 'punctuation', self.position, self.line_number)
-    
-  def tokenize(self):
-    self.position += 1
-
-    while not self.is_eof():
-      character = self.source_code[self.position]
-      if character.isdigit(): # tokenize_digit()
-        return self.number_tokenizer()
-      elif character.isalpha() or character == '_': #Identifier
-        return self.identifier_tokenizer()
-      elif character.isspace(): #Space
-        return self.whitespace_tokenizer()
-      elif character == '#': #Comment
-        return self.comment_tokenizer()
-      elif character == '+':
-        return self.plus_tokenizer()
-      elif character in self.punctuations.keys():
-        return self.punctuation_tokenizer()
-      elif character == '=':
-        return self.equal_tokenizer()
-      else:
-        raise Exception('Step Error[' + str(self.line_number) + ', ' + str(self.position) +']: unexpected token "' + character + '"')
-        
-      self.position += 1
-    return Token('EOF','EOF', 'EOF', self.position, self.line_number)
-
-tokenizer = Tokenizer("var int fileSize = 200;")
+tokenizer = Tokenizer(source_code)
 token = tokenizer.tokenize()
-while token.category != 'EOF':
-  print('token id ->', token.tid, 'category ->', token.category, 'value ->', token.value, 'position ->', token.position, 'line number ->', token.line_number)
-  token = tokenizer.tokenize()
+
+style = {
+  'keyword': 'color:blue',
+  'punctuation': 'color:red',
+  'operator': 'color:red',
+  'comment' : 'color:gray',
+  'identifier': 'color: #F94',
+  'literal': 'color:black; font-weight:bold',
+  'error' : 'color: white; background-color:red; padding: 8px;',
+  'whitespace': '',
+  'EOF': ''
+}
+
+with open('examples/main.html', 'w') as hstp:
+  hstp.write("<html><body><code><pre>")
+
+  while token.category != 'EOF':
+    hstp.write('<span style="'+style[token.category]+'">'+token.value+'</span>')
+    token = tokenizer.tokenize()
+
+  hstp.write("</pre></code></body></html>")
+  
+
   
