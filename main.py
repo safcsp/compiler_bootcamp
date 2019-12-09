d7c92400243c87bb03b9f53984ad392a4dcc7846
Ayman <ayman.alshanqiti@gmail.com>
2019-12-09 17:57:38 +0300

    refactoring code => create Tokenizer Class and move rules to the class as a methods

--- main.py
+++ main.py
@@ -1,96 +1,117 @@
 
 class Token:
-  def __init__(self, value, category, position, line_number):
+  def __init__(self,tid, value, category, position, line_number):
+    self.tid = tid
     self.value = value
     self.category = category
     self.position = position
     self.line_number = line_number
 
-#digit -> [0-9]+
-position = -1
-line_number = 1
-step_keywords = ['def', 'var', 'int', 'float', 'string', 'boolean', 'if', 'else', 'for', 'while', 'end']
-def tokenize(source_code):
-  global position
-  global line_number
-  global step_keywords
+class Tokenizer:
+  def __init__(self, source_code):
+    self.position = -1
+    self.line_number = 1
+    self.step_keywords = ['def', 'var', 'int', 'float', 'string', 'boolean', 'if', 'else', 'for', 'while', 'end']
+    self.source_code = source_code
+    self.length = len(self.source_code)
 
-  position += 1
 
-  length = len(source_code)
-  while position < length:
-    character = source_code[position]
-    if character.isdigit(): # tokenize_digit()
-      number_token = Token(character, 'literal', position, line_number)
-      position += 1
-      while(position < length):
-        character = source_code[position]
-        if not character.isdigit():
-          position -= 1
-          break
-        else: #isdigit
-          number_token.value += character
-          position += 1
+  def is_eof(self):
+    return not self.position < self.length
 
-      return number_token
-    elif character.isalpha() or character == '_': #Identifier
-      id_token = Token(character, 'identifier', position, line_number)
-      position += 1
-      while(position < length):
-        character = source_code[position]
-        if not (character.isalnum() or character == '_'):
-          position -= 1
-          break
-        else: #isid
-          id_token.value += character
-          position += 1
+  def number_tokenizer(self):
+    character = self.source_code[self.position]
+    token = Token('integer_literal', character, 'literal', self.position, self.line_number)
+    self.position += 1
+    while not self.is_eof():
+      character = self.source_code[self.position]
+      if not character.isdigit():
+        self.position -= 1
+        break
+      else: #isdigit
+        token.value += character
+        self.position += 1
 
-      if id_token.value in step_keywords:
-        id_token.category = 'keyword'
-      return id_token
-    elif character.isspace(): #Space
-      space_token = Token(character, 'space', position, line_number)
+    return token
+  
+  def identifier_tokenizer(self):
+    character = self.source_code[self.position]
+    token = Token('id', character, 'identifier', self.position, self.line_number)
+    self.position += 1
+    while not self.is_eof():
+      character = self.source_code[self.position]
+      if not (character.isalnum() or character == '_'):
+        self.position -= 1
+        break
+      else:
+        token.value += character
+        self.position += 1
       
+      if token.value in self.step_keywords:
+        token.category = 'keyword'
+        token.tid = token.value + '_keyword'
+        return token
+
+    return token
+  
+  def comment_tokenizer(self):
+    character = self.source_code[self.position]
+    token = Token('comment', character, 'comment', self.position, self.line_number)
+    self.position += 1
+    while not self.is_eof():
+      character = self.source_code[self.position]
       if character == '\n':
-        line_number += 1
-     
-      position += 1
-      while(position < length):
-        character = source_code[position]
-        if not (character.isspace()):
-          position -= 1
-          break
-        else: #isspace
-          if character == '\n':
-            line_number += 1
-          space_token.value += character
-          position += 1
+        self.position -= 1
+        break
+      else: 
+        token.value += character
+        self.position += 1
 
-      return space_token
-    elif character == '#': #Comment
-      comment_token = Token('', 'comment', position, line_number)
-      position += 1
-      while(position < length):
-        character = source_code[position]
+    return token
+  
+  def whitespace_tokenizer(self):
+    character = self.source_code[self.position]
+    token = Token('whitespace', character, 'whitespace', self.position, self.line_number)
+        
+    if character == '\n':
+      self.line_number += 1
+  
+    self.position += 1
+    while not self.is_eof():
+      character = self.source_code[self.position]
+      if not character.isspace():
+        self.position -= 1
+        break
+      else: #isspace
         if character == '\n':
-          position -= 1
-          break
-        else: #iscomment
-          comment_token.value += character
-          position += 1
+          self.line_number += 1
+        token.value += character
+        self.position += 1
 
-      return comment_token
-    else:
-      raise Exception('Step Error[' + str(line_number) + ', ' + str(position) +']: unexpected token "' + character + '"')
-      
-    position += 1
-  return Token('EOF', 'EOF', position, line_number)
+    return token
 
-#.............\n
-source_code = "def var size 10 #This is a comment var x = 2"
-token = tokenize(source_code)
+  def tokenize(self):
+    self.position += 1
+
+    while not self.is_eof():
+      character = self.source_code[self.position]
+      if character.isdigit(): # tokenize_digit()
+        return self.number_tokenizer()
+      elif character.isalpha() or character == '_': #Identifier
+        return self.identifier_tokenizer()
+      elif character.isspace(): #Space
+        return self.whitespace_tokenizer()
+      elif character == '#': #Comment
+        return self.comment_tokenizer()
+      else:
+        raise Exception('Step Error[' + str(self.line_number) + ', ' + str(self.position) +']: unexpected token "' + character + '"')
+        
+      self.position += 1
+    return Token('EOF','EOF', 'EOF', self.position, self.line_number)
+
+tokenizer = Tokenizer("def var size 10 #This is a comment var x = 2")
+token = tokenizer.tokenize()
 while token.category != 'EOF':
   print('category ->', token.category, 'value ->', token.value, 'position ->', token.position, 'line number ->', token.line_number)
-  token = tokenize(source_code)
+  token = tokenizer.tokenize()
   
-print(token.value)
